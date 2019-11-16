f0d36ef3dace2ea05d1df0b92538553ee2bf1f8b
Marcin <47139465+cacin@users.noreply.github.com>
2019-11-16 14:24:14 +0100

    Add files via upload

---
+++ IPI.Zajecia1/Zajecia.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPI.Zajecia1
+{
+  
+    class Zajecia
+    {
+        public string Nazwa { get; set; }
+        public string Prowadzacy { get; set; }
+        public string Sala { get; set; }
+        public bool CzyEgzamin { get; set; }
+        public TimeSpan Dlugosc { get; set; }
+        public DateTime GodzinaRozpoczecia { get; set; }
+
+        public DateTime GodzinaZakonczenia
+        {
+            get {
+                return GodzinaRozpoczecia.
+                    AddTicks(Dlugosc.Ticks);
+            }
+        }
+        public void WypiszZajecia()
+        {
+            Console.WriteLine("Nazwa: " + Nazwa);
+            Console.WriteLine("Sala: " + Sala);
+            Console.WriteLine("Godzina rozpoczęcia: " + GodzinaRozpoczecia);
+            Console.WriteLine("Godzina zakończenia: " + GodzinaZakonczenia);
+        }
+    }
+}
